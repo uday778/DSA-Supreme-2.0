d6e0a2e0cc7820f99af03909ae8b26797e57de37
uday kumar <uday33379@gmail.com>
2023-12-08 14:47:59 +0530

    2d-vector-notes

---
+++ Array-class-3/2D-vectors.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>>arr(5,vector<int>(10,-1));
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[i].size();j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    //jagged array==>> home work
+
+    vector<vector<int>>brr;
+    vector<int>vec1(10,0);
+    vector<int>vec2(8,1);
+    vector<int>vec3(6,2);
+    vector<int>vec4(4,3);
+    vector<int>vec5(7,-1);
+    brr.push_back(vec1);
+    brr.push_back(vec2);
+    brr.push_back(vec3);
+    brr.push_back(vec4);
+    brr.push_back(vec5);
+    for(int i=0;i<brr.size();i++){
+        for(int j=0;j<brr[i].size();j++){
+            cout<<brr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
